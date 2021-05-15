de7dda1413d5c8af8a955961becce0408f9b4502
LsHallo <xlshallox@gmail.com>
2021-05-16 00:56:32 +0200

    disable favicon animation on mobile; pretty pointless since it is invisible

--- js/calculator.js
+++ js/calculator.js
@@ -288,7 +288,7 @@ function calcRequest(hash, num_gpu) {
         if (parts.length === 2) return parts.pop().split(';').shift();
     }
 
-    function generateUid(length=24) {
+    function generateUid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
